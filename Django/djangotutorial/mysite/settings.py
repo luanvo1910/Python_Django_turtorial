895c76f98419a6a3e8ae7212d3b84cf2dd60bfe1
luanvo1910 <vothanhluan19102001@gmail.com>
2025-02-04 16:51:18 +0700

    use mysql database

--- Django/djangotutorial/mysite/settings.py
+++ Django/djangotutorial/mysite/settings.py
@@ -74,9 +74,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_turtorial',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
