c126f84cf4ce7a96f007aa10c6da165cea763223
luanvo1910 <vothanhluan19102001@gmail.com>
2025-02-04 17:42:22 +0700

    create model Category

--- Django/djangotutorial/news/models.py
+++ Django/djangotutorial/news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+LAYOUT_CHOICE = (
+    ('list', 'List'),
+    ('grid', 'Grid')
+)
+
+STATUS_CHOICE = (
+    ('draft', 'Draft'),
+    ('published', 'Published')
+)
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    slug = models.SlugField(unique=True, max_length=200)
+    is_homepage = models.BooleanField(default=False)
+    layout = models.CharField(max_length=10, choices=LAYOUT_CHOICE, default='list')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
+    ordering = models.IntegerField(default=0)
