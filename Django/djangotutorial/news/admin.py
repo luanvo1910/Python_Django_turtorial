b6290801b620646757ffe1947250297dd8c76b8d
luanvo1910 <vothanhluan19102001@gmail.com>
2025-02-04 17:55:02 +0700

    search and filter Category

--- Django/djangotutorial/news/admin.py
+++ Django/djangotutorial/news/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from .models import Category
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_homepage', 'layout', 'ordering')
-    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['is_homepage', 'status', 'layout']
+    search_fields = ['name']
+
 
 admin.site.register(Category, CategoryAdmin)
--- Django/djangotutorial/news/models.py
+++ Django/djangotutorial/news/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 
-LAYOUT_CHOICE = (
-    ('list', 'List'),
-    ('grid', 'Grid')
-)
-
-STATUS_CHOICE = (
-    ('draft', 'Draft'),
-    ('published', 'Published')
-)
-
 class Category(models.Model):
+    LAYOUT_CHOICE = (
+        ('list', 'List'),
+        ('grid', 'Grid')
+    )
+    STATUS_CHOICE = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
     name = models.CharField(unique=True, max_length=100)
     slug = models.SlugField(unique=True, max_length=200)
     is_homepage = models.BooleanField(default=False)
     layout = models.CharField(max_length=10, choices=LAYOUT_CHOICE, default='list')
     status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
     ordering = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
